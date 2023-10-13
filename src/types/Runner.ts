e07ec71de81c5a163d50050a68994c4dcb7707ba
gaozhijie <gzj941001@icloud.com>
2023-10-13 10:53:33 +0800

    update file and packages relate type

--- src/types/Runner.ts
+++ src/types/Runner.ts
@@ -1,3 +1,5 @@
+import { FileStats } from "./FileStats"
+
 export interface Runner {
   init: (
     stdout: (msg: string) => void,
@@ -13,14 +15,23 @@ export interface Runner {
     packages?: string[][]
   ) => Promise<void>
   interruptExecution: () => void
-  readFile: (name: string) => void
-  writeFile: (name: string, data: string) => void
+  readFile: (name: string) => string | ArrayBuffer
+  writeFile: (name: string, data: string|ArrayBufferView) => void
   mkdir: (name: string) => void
   rmdir: (name: string) => void
+  deleteFile: (path:string) => void
+  readdir: (path:string) => string[]
+  stat: (path:string) => FileStats
+  isDirExists: (path:string) => boolean
 }
 
 export interface PythonRunner extends Runner {
   run: (code: string) => Promise<void>
+  addPackages: (packages: string[]) => Promise<void>
+  loadedPackages: () => {
+    [key: string]: string
+  }
+  loadPackagesFromImports: (code: string) => Promise<void>
 }
 
 export interface PythonConsoleRunner extends Runner {
